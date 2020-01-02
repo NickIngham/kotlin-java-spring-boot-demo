49f0984e8c2fdfd2ccc365084654003fc8d4d760
Nick Ingham <>
2020-01-02 19:21:49 +0000

    adds the 2 spring apis to the repo

---
+++ kotlin/src/test/kotlin/com/kotlinjavademo/kotlin/KotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinjavademo.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
